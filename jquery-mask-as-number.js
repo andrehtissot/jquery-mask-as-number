948ff042ea8131a41823a47a22848ea2268828c4
Andre Augusto Tissot <andre_a_tissot@yahoo.com.br>
2016-12-09 15:20:59 -0200

    Added paste event support, removed useless stored data, improved max value handling

--- jquery-mask-as-number.js
+++ jquery-mask-as-number.js
@@ -1,5 +1,5 @@
 /*!
- * MaskAsNumber Jquery Plugin v1.0
+ * MaskAsNumber Jquery Plugin v1.1
  * https://github.com/andrehtissot/jquery-mask-as-number
  *
  * Requires jQuery Library
@@ -8,7 +8,7 @@
  * Copyright André Augusto Tissot
  * Released under the MIT license
  *
- * Date: 2016-12-04
+ * Date: 2016-12-09
  */
 (function($) {
   var compareIntegerStrings = function(integerStringA, integerStringB){
@@ -18,7 +18,7 @@
     if(integerStringB > integerStringA) { return 1; }
     return 0;
   }
-  var fixValue = function(originalValue, min, max){
+  var fixValue = function(originalValue, min, max, maxlength){
     var value = originalValue;
     value = value.replace(/\D/g,'');
     if(value !== '0') { value = value.replace(/^0+/,''); }
@@ -26,41 +26,37 @@
       value = min;
     else if(max && compareIntegerStrings(value, max) === -1)
       value = max;
+    else if(maxlength && value.length > maxlength)
+      value = value.substr(0, maxlength);
     return (''+value) === originalValue ? false : value;
   }
   $.fn.maskAsNumber = function(options){
     var options = options || {};
-    $(this).data('maskAsNumber', 'loaded')
-      .each(function(idx,element){
+    $(this).each(function(idx,element){
       var $elem = $(element), min = null, value = $elem.val(),
         max = (options.max || $elem.data('maskAsNumberMax') || null);
-      if (max === null && $elem.attr('maxlength'))
-        max = ((Math.pow(10,parseInt($elem.attr('maxlength'),10))-1) || null);
-      if(max !== null && !($elem.data('maskAsNumberMax')))
-        $elem.data('maskAsNumberMax', ''+max);
+      var maxlength = $elem.attr('maxlength') || null;
+      if (max !== null && maxlength === null)
+        $elem.attr('maxlength', (''+max).length);
       if(options.min){ $elem.data('maskAsNumberMin', min = options.min); }
       else if($elem.data('maskAsNumberMin'))
         min = $elem.data('maskAsNumberMin');
       if(!(/\d/.test(value))){
-        $elem.data('maskAsNumberOldValue', '').val('');
+        $elem.val('');
         return;
       }
-      var fixedValue = fixValue(value, min, max);
-      if(fixedValue === false)
-        $elem.data('maskAsNumberOldValue', $elem.val());
-      else $elem.data('maskAsNumberOldValue', fixedValue).val(fixedValue);
+      var fixedValue = fixValue(value, min, max, maxlength);
+      if(fixedValue !== false) { $elem.val(fixedValue); }
     }).on('keypress.maskAsNumber', function(event){
       if (event.ctrlKey || event.keyCode === 13
         || event.keyCode === 9 || event.charCode === 0) { return; }
       var char = event.char || String.fromCharCode(event.charCode);
       if (!(/^\d$/.test(char))) { event.preventDefault(); return; }
-      var $this = $(this), oldValue = $this.val();
-      $this.data('maskAsNumberOldValue', oldValue);
     }).on('keyup.maskAsNumber', function(event){
       var $this = $(this), value = $this.val();
       if(value === '' || value === '0') { return; }
       var fixedValue = fixValue(value, null,
-        ($this.data('maskAsNumberMax') || null));
+        ($this.data('maskAsNumberMax') || null), null);
       if (fixedValue !== false)
         $this.val(''+fixedValue);
     }).on('focusout.maskAsNumber', function(event){
@@ -68,9 +64,26 @@
       if(value === '') { return; }
       var fixedValue = fixValue(value,
         ($this.data('maskAsNumberMin') || null),
-        ($this.data('maskAsNumberMax') || null));
+        ($this.data('maskAsNumberMax') || null), null);
       if(fixedValue === '0') { return; }
       if(fixedValue !== false) { $this.val(fixedValue); }
+    }).on('paste.maskAsNumber', function(e){
+      var pastedString;
+      if(e.originalEvent.clipboardData){
+       pastedString = (e.originalEvent || e).clipboardData.getData('text/plain');
+      } else if(window.clipboardData){
+        pastedString = window.clipboardData.getData('Text');
+      } else { return; }
+      if(!/\D/.test(pastedString)){ return; }
+      var $this = $(this);
+      var maxlength = $this.attr('maxlength');
+      $this.removeAttr('maxlength');
+      setTimeout(function($elem, maxlength){
+        $elem.attr('maxlength', maxlength);
+        var value = $elem.val();
+        if(value.length > maxlength)
+          $elem.val(value.substr(0, maxlength));
+      }, 100, $this, maxlength);
     });
   };
 })(jQuery);
